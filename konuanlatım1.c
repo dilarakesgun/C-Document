00fa3a03cec8140d3d9cd03a0894747601f210c6
Dilara Kesgün <122830501+dilarakesgun@users.noreply.github.com>
2023-02-17 17:27:27 +0300

    Add files via upload

---
+++ konuanlatım1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+	
+	printf("Hello World \n");
+ 
+
+/*  "\n" bu iþaret yeni satýra atlamak için kullanýlýr.
+"\t" bu da tab yani bir boþluk býrakmak için kullanýlýr.
+
+DATA TYPES
+int tüm sayýlar(negatif,0,pozitif)
+
+float decimalli sayýlar (3.50,-4.5)(6 decimal places)
+
+double decimalli sayýlar (daha kapsamlý ince hesaplamalarda kullanýlýr.)(15 decimal places)
+
+char karakter */
+
+	int day=2;
+	printf("Today is the %d rd day of the week.",day);
+	
+	return 0;
+}
+	
+
+
+
+ 
